54f6f4480c977c996c3d45d57e4f0452de915f76
Yuran Simao <yuransimao7@gmail.com>
2023-04-15 22:59:49 +0100

    addicionando playList

--- JavaScript/Rocketseat/Spotify/spotify-interface-clone/src/app/page.js
+++ JavaScript/Rocketseat/Spotify/spotify-interface-clone/src/app/page.js
@@ -5,7 +5,14 @@ export default function Home() {
       <div className={`flex flex-1`}>
 
         <aside className={`w-72 bg-zinc-950 p-6`}>
-          <nav className={`space-y-5`}>
+
+          <div className={`flex items-center gap-2`}>
+            <div className={`w-3 h-3 bg-red-500 rounded-full`}/>
+            <div className={`w-3 h-3 bg-yellow-500 rounded-full`}/>
+            <div className={`w-3 h-3 bg-green-500 rounded-full`}/>
+          </div>
+
+          <nav className={`space-y-5 mt-10`}>
             <a href='#' className={`flex items-center gap-3 text-sm font-semibold text-zinc-200`}>
               <Homeicon/>
               Home
@@ -20,6 +27,26 @@ export default function Home() {
               Your library
               </a>
           </nav>
+          <nav className={`mt-10 pt-10 border-t border-zinc-800 flex flex-col gap-2`}>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+            <a href='#' className={`text-sm text-zinc-400`}>PlayList 1</a>
+
+      
+          </nav>
+
+          
           </aside>
 
         <main className={`flex-1 p-6`}>content</main>
