725a2678fb42350e9ab8ca20a77c768b53478d58
Yuran Simao <yuransimao7@gmail.com>
2023-04-15 22:45:09 +0100

    update

--- JavaScript/Rocketseat/Spotify/spotify-interface-clone/src/app/page.js
+++ JavaScript/Rocketseat/Spotify/spotify-interface-clone/src/app/page.js
@@ -1,14 +1,24 @@
-
+import {Home as Homeicon, Search, Library} from "lucide-react"
 export default function Home() {
   return (
     <div className={`h-screen flex flex-col`}>
       <div className={`flex flex-1`}>
 
         <aside className={`w-72 bg-zinc-950 p-6`}>
-          <nav>
-            <a href='#'>Home</a>
-            <a href='#'>Search</a>
-            <a href='#'>Your library</a>
+          <nav className={`space-y-5`}>
+            <a href='#' className={`flex items-center gap-3 text-sm font-semibold text-zinc-200`}>
+              <Homeicon/>
+              Home
+              </a>
+
+            <a href='#' className={`flex items-center gap-3 text-sm font-semibold text-zinc-200`}>
+              <Search/>
+              Search
+            </a>
+            <a href='#' className={`flex items-center gap-3 text-sm font-semibold text-zinc-200`}>
+              <Library/>
+              Your library
+              </a>
           </nav>
           </aside>
 
